a45ad7cf0773fd5ab78843464491c1a259165914
Paul Beeken <pbeeken@byramhills.org>
2022-04-10 13:32:25 -0400

    getting things warmed up getting external input

--- src/testing/main.cpp
+++ src/testing/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
+#include <Streaming.h>
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(A0, INPUT);
+
+  Serial.begin(115200);
+  
   // put your setup code here, to run once:
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  int reading = analogRead(A0);
+  Serial << "Voltage" << reading << endl;
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(10);
 }
